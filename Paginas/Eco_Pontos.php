e6e2d20171efb1c457692d62b8bfe99fdb8cb577
Professor <Professor@192.168.200.173>
2016-10-14 16:12:37 -0200

    1ºUplond

--- Paginas/Eco_Pontos.php
+++ Paginas/Eco_Pontos.php
@@ -32,7 +32,7 @@
             <li><a href="#">Material Educacional</a></li>
           </ul>
         </li>
-       <li><a href="Mostra_Produto_s_Login.php">Trocas</a></li>
+        <li><a href="Mostra_Produto_s_Login.php">Trocas</a></li>
         <li><a href="Eco_Pontos.php">Ecopontos</a></li>
       </ul>
          <ul class="nav navbar-nav navbar-right">
@@ -46,42 +46,28 @@
 <div class="container">
   <div class="row">
       <div class="col-sm-12">
-          <div class="container">
-  <!--<h2>Responsive Embed</h2>-->
-  <div class="embed-responsive embed-responsive-16by9">
-      <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/g56IGyh9FsA"></iframe>
-  </div>
-</div>
+          <div class="col-sm-1"></div> 
+          <div class="col-sm-4">
+              <div class="panel panel-default">
+         <form action="index.php">
+    <button type="submit" style="height:100%;width:100%;">            
+    <div class="panel-heading">Senac-Gravataí</div>
+   
+    <div class="panel-body">
+  
+      <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3457.396381393985!2d-51.00047098443349!3d-29.93927518192075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95190b471d555555%3A0x2ea6a888bb7c3eb0!2sSenac!5e0!3m2!1spt-BR!2sbr!4v1476467404101" width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
       </div>
-    <div class="col-sm-4">
-      <h3>Column 1</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 2</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 3</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation</p>
-    </div>
+    </button>
+    </form>
+            
   </div>
-</div>
-    
-    <script>
-        
-    </script>
+          <div class="col-sm-1"></div>
+          <div class="col-sm-4">
+              
+          </div> 
+          <div class="col-sm-1"></div> 
+  <!--<h2>Responsive Embed</h2>-->
+
 </body>
 </html>
 
